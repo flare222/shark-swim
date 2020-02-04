5263f8e5aa01d2a116ad62a36af2917ad784d12b
Clare Robertson <clare.robertson@gaincredit.com>
2020-02-04 11:20:38 +0000

    deploy

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
-const port = 4000
-const dbURI = 'mongodb://localhost/shark-swim-api'
-const secret = 'swim with more sharks'
+const port = process.env.PORT || 4000
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/shark-swim-api'
+const secret = process.env.SECRET || 'swim with more sharks'
 
 module.exports = { port, dbURI, secret }
--- index.js
+++ index.js
@@ -14,11 +14,15 @@ mongoose.connect(
     console.log('We have Mongo')
   })
 
+app.use(express.static(`${__dirname}/dist`))
+
 app.use(bodyParser.json())
 
 app.use(logger)
 
 app.use('/api', router)
+
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`))
 
